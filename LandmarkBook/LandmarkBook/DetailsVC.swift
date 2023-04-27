c57e5a596f28a648fc1162b501d600265ca9a432
BartugKaan <bartugkaan.celebi@outlook.com>
2023-04-27 13:55:07 +0300

    LandmarkBook : creted new view controller

---
+++ LandmarkBook/LandmarkBook/DetailsVC.swift
@@ -0,0 +1,39 @@
+//
+//  DetailsVC.swift
+//  LandmarkBook
+//
+//  Created by Bartuğ Kaan Çelebi on 27.04.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var landmarkLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedLandmarkName = ""
+    var selectedLandmarkImage = UIImage()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        landmarkLabel.text = selectedLandmarkName
+        imageView.image = selectedLandmarkImage
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
