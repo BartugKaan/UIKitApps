ad887d6bdcdc02c0c80b00ab6d23be1ac06ac1db
BartugKaan <bartugkaan.celebi@outlook.com>
2023-05-01 16:40:21 +0300

    ArtBookProject : Design added

---
+++ ArtBookProject/ArtBookProject/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  ArtBookProject
+//
+//  Created by Bartuğ Kaan Çelebi on 1.05.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var artistText: UITextField!
+    
+    @IBOutlet weak var yearText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+    
+    @IBAction func saveButtonClicked(_ sender: UIButton) {
+    }
+}
