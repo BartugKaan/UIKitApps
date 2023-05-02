22e796477d42e797238df7dc276c9b08a810602b
BartugKaan <bartugkaan.celebi@outlook.com>
2023-05-02 13:33:37 +0300

    CurrencyConverter : added design

---
+++ CurencyConverter/CurencyConverter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CurencyConverter
+//
+//  Created by Bartuğ Kaan Çelebi on 2.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLAbel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLAbel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
