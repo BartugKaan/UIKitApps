93da813fccdd1f7cf63340b6682d95b740b79400
BartugKaan <bartugkaan.celebi@outlook.com>
2023-05-10 18:41:46 +0300

    Tipsy : modified stepper

--- Tipsy-iOS13-master/Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy-iOS13-master/Tipsy/Controllers/CalculatorViewController.swift
@@ -13,7 +13,6 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
-    
     @IBOutlet weak var splitNumberLabel: UILabel!
     
     
@@ -35,6 +34,8 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = Int(sender.value).description
+        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
@@ -47,6 +48,7 @@ class CalculatorViewController: UIViewController {
         else if twentyPctButton.isSelected{
             print("0.2")
         }
+        print(splitNumberLabel.text!)
     }
     
 }
