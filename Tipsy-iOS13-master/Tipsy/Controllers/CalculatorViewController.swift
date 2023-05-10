a644e3ad295daac9127760f39278a05a2b360048
BartugKaan <bartugkaan.celebi@outlook.com>
2023-05-10 17:53:23 +0300

    Tipsy : controller classes added

--- Tipsy-iOS13-master/Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy-iOS13-master/Tipsy/Controllers/CalculatorViewController.swift
@@ -1,9 +1,9 @@
 //
-//  ViewController.swift
+//  CalculatorViewController.swift
 //  Tipsy
 //
-//  Created by Angela Yu on 09/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Bartuğ Kaan Çelebi on 10.05.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
 //
 
 import UIKit
@@ -11,23 +11,27 @@ import UIKit
 class CalculatorViewController: UIViewController {
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
-    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var tenPctValue: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
+    
     @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func tipChanged(_ sender:UIButton){
-        
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
+    
     @IBAction func calculatePressed(_ sender: UIButton) {
     }
+    
 }
-
---
+++ Tipsy-iOS13-master/Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Bartuğ Kaan Çelebi on 10.05.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
