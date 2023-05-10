c2968fdbf4eebc961e986345f31e33105cbe96c3
BartugKaan <bartugkaan.celebi@outlook.com>
2023-05-10 18:35:03 +0300

    Tipsy : modified tipChanged function

--- Tipsy-iOS13-master/Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy-iOS13-master/Tipsy/Controllers/CalculatorViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class CalculatorViewController: UIViewController {
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
-    @IBOutlet weak var tenPctValue: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     
     @IBOutlet weak var splitNumberLabel: UILabel!
@@ -25,13 +25,28 @@ class CalculatorViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        if zeroPctButton.isSelected{
+            print("0")
+        }
+        else if tenPctButton.isSelected{
+            print("0.1")
+        }
+        else if twentyPctButton.isSelected{
+            print("0.2")
+        }
     }
     
 }
