3a20de964a97f6060ebba0dcbdab6f91e4973703
BartugKaan <bartugkaan.celebi@outlook.com>
2023-05-16 18:19:24 +0300

    Clima : protocols and delegates example finished

--- Playgrounds/Protocols and Delegates/Protocols and Delegates/main.swift
+++ Playgrounds/Protocols and Delegates/Protocols and Delegates/main.swift
@@ -1,11 +1,56 @@
-//
-//  main.swift
-//  Protocols and Delegates
-//
-//  Created by Bartuğ Kaan Çelebi on 16.05.2023.
-//
+protocol AdvanceLifeSupport{
+    func performCPR()
+}
 
-import Foundation
 
-print("Hello, World!")
+class EmergencyCallHandler{
+    var delegate: AdvanceLifeSupport?
+    
+    func assessSituation(){
+        print("Can you tell me what happend?")
+    }
+    func medicalEmergency(){
+        delegate?.performCPR()
+    }
+}
+
+struct Paramedic: AdvanceLifeSupport{
+    
+    init(handler: EmergencyCallHandler){
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The paramedic does chest compressions, 30 per second.")
+    }
+}
+
+class Doctor : AdvanceLifeSupport{
+    
+    init(handler : EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The doctor does chest compressions, 30 per second.")
+    }
+    
+    func useStetescope(){
+        print("Listining for heart sounds")
+    }
+}
+
+class Surgeon : Doctor{
+    override func performCPR() {
+        super.performCPR()
+        print("Signs staying alive by the BeeGees")
+    }
+}
+
+
+let emilio = EmergencyCallHandler()
+let angela = Surgeon(handler: emilio)
+
+
+emilio.assessSituation()
+emilio.medicalEmergency()
 
