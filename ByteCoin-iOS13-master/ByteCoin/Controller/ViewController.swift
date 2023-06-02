d96500cfc3536b7aa9204d5e6b372c501fd73322
BartugKaan <bartugkaan.celebi@outlook.com>
2023-06-02 23:19:45 +0300

    ByteCoin : VC Refactoring

--- ByteCoin-iOS13-master/ByteCoin/Controller/ViewController.swift
+++ ByteCoin-iOS13-master/ByteCoin/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate{
+class ViewController: UIViewController{
     
     
     
@@ -25,7 +25,26 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         currencyPicker.delegate = self
         // Do any additional setup after loading the view.
     }
+}
+
+//MARK: - CoinManagerDelegate
+extension ViewController: CoinManagerDelegate {
     
+    func didUpdatePrice(price: String, currency: String) {
+        
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+//MARK: - UIPickerViewDelegate and Data Source
+extension ViewController : UIPickerViewDelegate, UIPickerViewDataSource{
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -41,20 +60,4 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         coinManager.getCoinPrice(for: coinManager.currencyArray[row])
     }
-    
-    
-    func didUpdatePrice(price: String, currency: String) {
-        DispatchQueue.main.async {
-            self.bitcoinLabel.text = price
-            self.currencyLabel.text = currency
-        }
-    }
-    
-    func didFailWithError(error: Error) {
-        print(error)
-    }
-    
-
-
 }
-
