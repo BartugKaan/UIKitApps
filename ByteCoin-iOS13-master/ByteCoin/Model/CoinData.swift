0f443fc8741cc7297d68da0e85caa43dc4b27bf7
BartugKaan <bartugkaan.celebi@outlook.com>
2023-06-02 22:58:03 +0300

    ByteCoin : Parsing Json

---
+++ ByteCoin-iOS13-master/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Bartuğ Kaan Çelebi on 2.06.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData: Codable{
+    let rate : Double
+}
--- ByteCoin-iOS13-master/ByteCoin/Model/CoinManager.swift
+++ ByteCoin-iOS13-master/ByteCoin/Model/CoinManager.swift
@@ -14,33 +14,48 @@ struct CoinManager {
     let apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    
     func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        
+        if let url = URL(string: urlString) {
             
-            //Use String concatenation to add the selected currency at the end of the baseURL along with the API key.
-            let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
             
-            //Use optional binding to unwrap the URL that's created from the urlString
-            if let url = URL(string: urlString) {
-                
-                //Create a new URLSession object with default configuration.
-                let session = URLSession(configuration: .default)
-                
-                //Create a new data task for the URLSession
-                let task = session.dataTask(with: url) { (data, response, error) in
-                    if error != nil {
-                        print(error!)
-                        return
-                    }
-                    //Format the data we got back as a string to be able to print it.
-                    let dataAsString = String(data: data!, encoding: .utf8)
-                    print(dataAsString!)
-                    
+            let session = URLSession(configuration: .default)
+            
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
                 }
-                //Start task to fetch data from bitcoin average's servers.
-                task.resume()
+                
+                if let safeData = data{
+                    let bitcoinRate = parseJSON(safeData)
+                }
             }
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double?{
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            print(lastPrice)
+            return lastPrice
+        }
+        catch{
+            print(error)
+            return nil
+        }
     }
 }
-    
+
+
+
+
 
